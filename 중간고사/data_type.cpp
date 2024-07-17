c2a8d0d5cff3f4ddf9ee6dc7bf82a37250d5a7ff
Dobby19 <150355097+FrontHeadlock@users.noreply.github.com>
2024-07-17 14:06:38 +0900

    Add files via upload

---
+++ 중간고사/data_type.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    int x=10;
+    char c;
+    double d=12345.12345;
+
+    printf("%5d\n",x);
+    printf("%.3f\n",d);
+
+
+    printf("%c\n",'a');
+    printf("%s\n","HELLO WORLD!");
+
+    printf("%%\n");
+    printf("\" \'\n");
+
+
+    char code1=65;
+    char code2='A';
+
+    printf("%c\n",code1);
+    printf("%.3f\n",7/(double)4);
+
+    
+
+    return 0;
+}
---
+++ 중간고사/별찍기_5.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    for(int i=0;i<5;i++){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
